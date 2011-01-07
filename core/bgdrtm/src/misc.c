41f2c33006ed1145d65da2b78262cd1d13c55917
SplinterGU <SplinterGU@3fa62efa-c05c-4319-b09d-9b630d73c7a0>
2011-01-07 00:12:15 +0000

    - Fix speed problem in differents caanoo firmware version
    git-svn-id: https://svn.code.sf.net/p/bennugd/code@202 3fa62efa-c05c-4319-b09d-9b630d73c7a0

--- core/bgdrtm/src/misc.c
+++ core/bgdrtm/src/misc.c
@@ -114,12 +114,23 @@ int debug     = 0;  /* 1 if running in debug mode      */
 volatile unsigned long *__bgdrtm_memregl = NULL;
 int __bgdrtm_memdev = -1;
 
+#ifdef TARGET_CAANOO
+static unsigned long caanoo_firmaware_version = 0;
+#endif
+
 void bgdrtm_ptimer_init(void)
 {
 #if defined(TARGET_GP2X_WIZ)
     TIMER_REG(0x44) = 0x922;
 #else
-    TIMER_REG(0x44) = 0x0FF2;
+    if ( caanoo_firmaware_version < 1000006 ) /* firmware version < 1.0.6 */
+    {
+        TIMER_REG(0x44) = 0x922;
+    }
+    else
+    {
+        TIMER_REG(0x44) = 0x0FF2;
+    }
 #endif
     TIMER_REG(0x40) = 0x0c;
     TIMER_REG(0x08) = 0x6b;
@@ -189,6 +200,32 @@ void bgdrtm_entry( int argc, char * argv[] )
         GLODWORD( OS_ID ) = _OS_ID ;
 
 #if defined(TARGET_GP2X_WIZ) || defined(TARGET_CAANOO)
+
+    {
+        FILE * fp = fopen( "/usr/gp2x/version", "r" );
+        if ( fp )
+        {
+            char *p1, *p2;
+            char b[32] = "";
+
+            fgets( b, sizeof(b), fp );
+            fclose( fp );
+
+            if ( ( p2 = strchr( b, '.' ) ) )
+            {
+                *p2++ = '\0';
+                caanoo_firmaware_version = atol( b ) * 1000000L;
+                if ( ( p1 = strchr( p2, '.' ) ) )
+                {
+                    *p1++ = '\0';
+                    caanoo_firmaware_version = atol( p2 ) * 1000L;
+                    caanoo_firmaware_version = atol( p1 );
+
+                }
+            }
+        }
+    }
+
     __bgdrtm_memdev = open( "/dev/mem", O_RDWR );
     __bgdrtm_memregl = mmap( 0, 0x20000, PROT_READ|PROT_WRITE, MAP_SHARED, __bgdrtm_memdev, 0xc0000000 );
 
