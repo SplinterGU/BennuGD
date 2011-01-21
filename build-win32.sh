05f45743ba18a3dd33ef1b9ddfa3804f82545c43
SplinterGU <SplinterGU@3fa62efa-c05c-4319-b09d-9b630d73c7a0>
2011-01-21 05:42:54 +0000

    - Add build-win32.sh
    git-svn-id: https://svn.code.sf.net/p/bennugd/code@224 3fa62efa-c05c-4319-b09d-9b630d73c7a0

---
+++ build-win32.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+TARGET=gnu-win32
+
+#echo "### Building 3rd party software ###"
+#cd 3rdparty/des-4.04b
+#make clean
+#make
+#cd -
+
+echo "### Building BennuGD Core ###"
+
+cd core
+./configure
+make clean
+make
+cd -
+
+echo "### Building BennuGD Modules ###"
+
+cd modules
+./configure
+make clean
+make
+cd -
+
+echo "### Building BennuGD Tools ###"
+
+cd tools/moddesc
+./configure
+make clean
+make
+cd -
+
+echo "### Copying files to bin folder ###"
+
+mkdir -p bin/$TARGET 2>/dev/null
+#cp 3rdparty/des-4.04b/libdes.dll bin/$TARGET
+cp core/bgdi/src/.libs/bgdi.exe bin/$TARGET
+cp core/bgdc/src/bgdc.exe bin/$TARGET
+cp core/bgdrtm/src/.libs/libbgdrtm.dll bin/$TARGET
+cp modules/mod*/.libs/mod*.dll bin/$TARGET
+cp modules/lib*/.libs/lib*.dll bin/$TARGET
+cp tools/moddesc/moddesc.exe bin/$TARGET
+
+echo "### Build done! ###"
