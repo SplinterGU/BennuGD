89d7b050e5019f173c69d3285f2a364f257bf13f
splintergu <splintergu@3fa62efa-c05c-4319-b09d-9b630d73c7a0>
2013-01-06 01:50:56 +0000

    - copyright update
    git-svn-id: https://svn.code.sf.net/p/bennugd/code@317 3fa62efa-c05c-4319-b09d-9b630d73c7a0

--- build-static.sh
+++ build-static.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright © 2006-2012 SplinterGU (Fenix/Bennugd)
+# Copyright © 2006-2013 SplinterGU (Fenix/Bennugd)
 #
 # This file is part of Bennu - Game Development
 #
--- modules/mod_text/mod_text_exports.h
+++ modules/mod_text/mod_text_exports.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright © 2006-2012 SplinterGU( Fenix/Bennugd )
+ *  Copyright © 2006-2013 SplinterGU( Fenix/Bennugd )
  *  Copyright © 2002-2006 Fenix Team( Fenix )
  *  Copyright © 1999-2002 José Luis Cebrián Pagüe( Fenix )
  *
